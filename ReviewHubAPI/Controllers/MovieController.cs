414958f1a221cd674a0bb1c1cf5e7dd7b5203499
Sakaria Guled <sakaria.guled@gmail.com>
2024-03-14 15:24:54 +0100

    La til movieController, service og repository

---
+++ ReviewHubAPI/Controllers/MovieController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReviewHubAPI.Models.DTO;
+using ReviewHubAPI.Services.Interface;
+
+namespace ReviewHubAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : Controller
+    {
+        private readonly IMovieService _movieservice;
+
+        public MovieController(IMovieService movieservice)
+        {
+            _movieservice = movieservice;
+        }
+        [HttpGet(Name = "GetAllMovies")]
+
+        public async Task<ActionResult< ICollection<MovieDTO>>> GetAllMovies()
+        {
+            var movies =  await _movieservice.GetAllMovies();
+
+            if (movies == null)
+            { 
+                return NotFound("There are no movies in the database"); 
+            }
+
+            return Ok(movies);
+        }
+
+        [HttpGet("{Id}", Name = "GetMovieById")]
+
+        public async Task<ActionResult<MovieDTO>> GetMovieById(int Id)
+        {
+            var movie = await _movieservice.GetMovieById(Id);
+            if (movie == null)
+            {
+                return NotFound("No movie with that id was found");
+            }
+            return Ok(movie);
+        }
+
+        // POST: MovieController/Edit/5
+        [HttpPut("{Id}", Name = "UpdateMovieById")]
+
+        public async Task<ActionResult<MovieDTO>> UpdateMovieById(int Id, MovieDTO dto)
+        {
+            var movie = await _movieservice.UpdateMovieById(Id, dto);
+            if (movie == null)
+            {
+                return BadRequest("The movie could not be updated");
+            }
+            return Ok(movie);
+        }
+
+        
+        [HttpDelete("{Id}", Name ="DeleteMovieById")]
+        public async Task<ActionResult<MovieDTO>> DeleteMovieById(int Id)
+        {
+            var movie = await _movieservice.DeleteMovieById(Id);
+
+            if (movie == null)
+            {
+                return BadRequest("The movie could not be deleted");
+            }
+            return Ok(movie);
+
+        }
+    }
+}
---
+++ ReviewHubAPI/Services/MovieService.cs
@@ -0,0 +1,74 @@
+﻿using ReviewHubAPI.Mappers.Interface;
+using ReviewHubAPI.Models.DTO;
+using ReviewHubAPI.Models.Entity;
+using ReviewHubAPI.Repositories.Interface;
+using ReviewHubAPI.Services.Interface;
+
+namespace ReviewHubAPI.Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _movierep;
+        private readonly IMapper<MovieEntity, MovieDTO> _moviemapper;
+
+        public MovieService(IMovieRepository movierep, IMapper<MovieEntity, MovieDTO> moviemapper)
+        {
+            _movierep = movierep;
+            _moviemapper = moviemapper;
+        }
+        public async Task<MovieDTO> DeleteMovieById(int Id)
+        {
+            var movie = await _movierep.GetMovieById(Id);
+
+            if (movie != null)
+            {
+                await _movierep.DeleteMovieById(Id);
+
+            }
+
+            return _moviemapper.MapToDTO(movie!);
+
+        }
+
+        public async Task<ICollection<MovieDTO>> GetAllMovies()
+        {
+            var movies = await  _movierep.GetAllMovies();
+            ICollection<MovieDTO> moviesdto = new List<MovieDTO>();
+
+            foreach (var movie in movies)
+            {
+                moviesdto.Add(_moviemapper.MapToDTO(movie));
+            }
+
+            return moviesdto ?? null!;
+        }
+
+        public async Task<MovieDTO> GetMovieById(int Id)
+        {
+            var movie = await _movierep.GetMovieById(Id);
+
+            return _moviemapper.MapToDTO(movie) ?? null!;   
+        }
+
+        public async Task<MovieDTO> UpdateMovieById(int Id, MovieDTO dto)
+        {
+            var foundmovie = await  _movierep.GetMovieById(Id);
+
+            var dateupdated = DateTime.Now;
+
+            if (foundmovie != null)
+            {
+                foundmovie.Id = dto.Id;
+                foundmovie.MovieName = dto.MovieName;
+                foundmovie.ReleaseYear = dto.ReleaseYear;
+                foundmovie.Director = dto.Director;
+                foundmovie.Genre = dto.Genre;
+                foundmovie.DateUpdated = dateupdated;
+
+            }
+
+            return _moviemapper.MapToDTO(foundmovie!);
+                        
+        }
+    }
+}
