9f3204227d80ddc14f7b85df7d3c273c580f2400
Sakaria Guled <sakaria.guled@gmail.com>
2024-04-01 23:11:28 +0200

    La til moviesiden

---
+++ HTML/MoviePage/app.js
@@ -0,0 +1,5 @@
+
+var backgroundchange = document.getElementById("background")
+
+backgroundchange.style.backgroundImage = 'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.8)),url("../assets/Background/nocturnal-animals.jpg")'
+backgroundchange.style.backgroundSize = 'Cover'
