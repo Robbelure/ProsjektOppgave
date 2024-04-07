da93ee123b73389aa847856bd6828eab65953298
Sakaria Guled <sakaria.guled@gmail.com>
2024-04-07 21:12:58 +0200

    addMoviepage

--- HTML/MoviePage/app.js
+++ HTML/MoviePage/app.js
@@ -1,5 +1,5 @@
 
 var backgroundchange = document.getElementById("background")
 
-backgroundchange.style.backgroundImage = 'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.8)),url("../assets/Background/nocturnal-animals.jpg")'
+backgroundchange.style.backgroundImage = 'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.8)),url("../assets/Background/1.JPG")'
 backgroundchange.style.backgroundSize = 'Cover'
