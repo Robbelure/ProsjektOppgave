90fecb339cb32b45209fefd94cdd0383918cf199
Robbelure <robinliverpool@live.no>
2024-04-19 16:39:52 +0200

    Registrering og omdirigering til bekreftelsesside(her kan vi legge til bekreftelse via epost hvis vi rekker)

---
+++ HTML/ConfirmationPage/app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Logikk for å bekrefte at brukeren har kommet til denne siden etter en vellykket registrering
+    // kan inkluderes her, om nødvendig.
+});
